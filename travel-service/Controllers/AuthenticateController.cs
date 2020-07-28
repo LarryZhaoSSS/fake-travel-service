7097a464105ca59dff3be17012c752becb7d93bf
LarryKameZhao <larryzhao1994@outlook.com>
2020-07-28 08:27:21 +0800

    generate token

---
+++ travel-service/Controllers/AuthenticateController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using travel_service.Dtos;
+
+namespace travel_service.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult login([FromBody] LoginDto loginDto)
+        {
+            // 1 验证用户名密码
+
+            // 2 创建jwt
+            // header
+            var signingAlgorithm = SecurityAlgorithms.HmacSha256;
+            // payload
+            var claims = new[]
+            {
+                // sub
+                new Claim(JwtRegisteredClaimNames.Sub, "fake_user_id")
+            };
+            // signiture
+            var secretByte = Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]);
+            var signingKey = new SymmetricSecurityKey(secretByte);
+            var signingCredentials = new SigningCredentials(signingKey, signingAlgorithm);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Authentication:Issuer"],
+                audience: _configuration["Authentication:Audience"],
+                claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials
+            );
+
+            var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
+
+            // 3 return 200 ok + jwt
+            return Ok(tokenStr);
+        }
+    }
+}
---
+++ travel-service/Dtos/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace travel_service.Dtos
+{
+    public class LoginDto
+    {
+        public LoginDto()
+        {
+        }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}

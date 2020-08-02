a8aaadad0db4357b4a6fd488dd67307c4fb117a4
LarryKameZhao <larryzhao1994@outlook.com>
2020-08-02 16:33:35 +0800

    shoping cart and line item

---
+++ travel-service/Migrations/20200802083157_ShoppingCartMigration.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace travel_service.Migrations
+{
+    public partial class ShoppingCartMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCarts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LineItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TouristRouteId = table.Column<Guid>(nullable: false),
+                    ShoppingCartId = table.Column<Guid>(nullable: true),
+                    OriginalPrice = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    DiscountPresent = table.Column<double>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LineItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LineItems_ShoppingCarts_ShoppingCartId",
+                        column: x => x.ShoppingCartId,
+                        principalTable: "ShoppingCarts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LineItems_TouristRoutes_TouristRouteId",
+                        column: x => x.TouristRouteId,
+                        principalTable: "TouristRoutes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "308660dc-ae51-480f-824d-7dca6714c3e2",
+                column: "ConcurrencyStamp",
+                value: "7bc579a3-b3c1-4501-a4c4-3567e394ee21");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "90184155-dee0-40c9-bb1e-b5ed07afc04e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "1fcdc6a2-b562-4628-87c3-9c8302510ac2", "AQAAAAEAACcQAAAAEFp2FdJWJ63kLMWwQ9Pm+t+L4wDWNojbF5c2Twl1iorpPIfzDzja4gfQCnGtPQ5o1w==", "7b87149c-7631-471b-961f-93d0a7c0ece5" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineItems_ShoppingCartId",
+                table: "LineItems",
+                column: "ShoppingCartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineItems_TouristRouteId",
+                table: "LineItems",
+                column: "TouristRouteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCarts_UserId",
+                table: "ShoppingCarts",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LineItems");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "308660dc-ae51-480f-824d-7dca6714c3e2",
+                column: "ConcurrencyStamp",
+                value: "7dc29669-de62-4511-8245-b76fddad5c82");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "90184155-dee0-40c9-bb1e-b5ed07afc04e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c98afbde-4e90-4dbe-af3f-d6050fa63254", "AQAAAAEAACcQAAAAED4ZeiEGOvP3hKBjxrm1NE/xKIM+I4MUGpuquy5+hw13nYcdKleHD6mT46eZTUinkA==", "50a92554-3cab-4cdd-87aa-0015cb491205" });
+        }
+    }
+}

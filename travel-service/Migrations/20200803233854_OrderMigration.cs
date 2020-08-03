f827281c9b17f53f01f8ec7199c42b5c64e9eef8
LarryKameZhao <larryzhao1994@outlook.com>
2020-08-04 07:43:13 +0800

    order model

---
+++ travel-service/Migrations/20200803233854_OrderMigration.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace travel_service.Migrations
+{
+    public partial class OrderMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderId",
+                table: "LineItems",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    State = table.Column<int>(nullable: false),
+                    CreateDateUTC = table.Column<DateTime>(nullable: false),
+                    TransactionMetadata = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "308660dc-ae51-480f-824d-7dca6714c3e2",
+                column: "ConcurrencyStamp",
+                value: "c1d25878-e31a-495e-8df1-aaae85155978");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "90184155-dee0-40c9-bb1e-b5ed07afc04e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d0b56669-f40f-437f-8a8e-984e704ef57d", "AQAAAAEAACcQAAAAEGnu9ZnbAQ9VJfWGY7XcQ3KIg9I9rPxzn8SF32txZOWqfz1FaEXuHDtoXua2d0pz3g==", "d5f035b7-f5ea-4d36-8a1c-c9dfa50f94fe" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LineItems_OrderId",
+                table: "LineItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LineItems_Orders_OrderId",
+                table: "LineItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LineItems_Orders_OrderId",
+                table: "LineItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LineItems_OrderId",
+                table: "LineItems");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "LineItems");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "308660dc-ae51-480f-824d-7dca6714c3e2",
+                column: "ConcurrencyStamp",
+                value: "7bc579a3-b3c1-4501-a4c4-3567e394ee21");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "90184155-dee0-40c9-bb1e-b5ed07afc04e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "1fcdc6a2-b562-4628-87c3-9c8302510ac2", "AQAAAAEAACcQAAAAEFp2FdJWJ63kLMWwQ9Pm+t+L4wDWNojbF5c2Twl1iorpPIfzDzja4gfQCnGtPQ5o1w==", "7b87149c-7631-471b-961f-93d0a7c0ece5" });
+        }
+    }
+}
